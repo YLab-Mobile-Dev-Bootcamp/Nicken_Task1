a95343e469fad3655458382e57697e115196df8a
nickenmardhany <nickenkm99@gmail.com>
2021-08-21 01:45:23 +0700

    create a page contain 4 widget

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## TASK 2
  
 ![Recipe Food App Screenshot](asset/food.PNG)
+
+## TASK 3
+ 
+![Recipe Food App Screenshot](asset/list_food.PNG)
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app2/models/recipe.api.dart';
+import 'package:flutter_app2/models/recipe.dart';
 import 'package:flutter_app2/views/widgets/recipe_card.dart';
 
 class HomePage extends StatefulWidget {
@@ -7,6 +9,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Recipe> _recipes;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    getRecipes();
+  }
+
+  Future<void> getRecipes() async {
+    _recipes = await RecipeApi.getRecipe();
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,15 +34,21 @@ class _HomePageState extends State<HomePage> {
             children: [
               Icon(Icons.restaurant_menu),
               SizedBox(width: 10),
-              Text('Food Recipes'),
+              Text('Food Recipe')
             ],
           ),
         ),
-        body: RecipeCard(
-          title: 'My recipe',
-          rating: '4.9',
-          cookTime: '30 min',
-          thumbnailUrl: 'https://lh3.googleusercontent.com/ei5eF1LRFkkcekhjdR_8XgOqgdjpomf-rda_vvh7jIauCgLlEWORINSKMRR6I6iTcxxZL9riJwFqKMvK0ixS0xwnRHGMY4I5Zw=s360',
+        body: _isLoading
+            ? Center(child: CircularProgressIndicator())
+            : ListView.builder(
+          itemCount: _recipes.length,
+          itemBuilder: (context, index) {
+            return RecipeCard(
+                title: _recipes[index].name,
+                cookTime: _recipes[index].totalTime,
+                rating: _recipes[index].rating.toString(),
+                thumbnailUrl: _recipes[index].images);
+          },
         ));
   }
 }
